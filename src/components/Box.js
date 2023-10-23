0926ad73771e44de05972b75c802a1d03802a839
Yash Modi <Yash.Modi@ATMECS.CORP>
2023-10-23 16:23:49 +0530

    Comment Removed

--- src/components/Box.js
+++ src/components/Box.js
@@ -5,21 +5,14 @@ import FmdBadIcon from '@mui/icons-material/FmdBad';
 import { calculateDaysRemaining } from './utils';
 
 export const Box = (props) => {
-
     const targetDate = new Date(props.item.created_at);
     const currentDate = new Date();
     const [daysRemaining, setDaysRemaining] = useState(0);
 
     useEffect(() => {
-        const days=calculateDaysRemaining(targetDate,currentDate);
-        setDaysRemaining(days)
+        const days = calculateDaysRemaining(targetDate, currentDate);
+        setDaysRemaining(days);
     }, [targetDate]);
-
-    // const calculateDaysRemaining = () => {
-    //     const timeDifference = currentDate - targetDate;
-    //     const daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
-    //     setDaysRemaining(daysRemaining);
-    // };
 
     const handleButtonClick = () => {
         window.open(`${props.item.html_url}`, "_blank", "noreferrer");
@@ -36,13 +29,19 @@ export const Box = (props) => {
                         <Typography className='header'>{props.item.name}</Typography>
                         <Typography className='description'>{props.item.description}</Typography>
                         <div className="badgeContainer">
-                            <Chip className="chip" avatar={<Avatar><StarIcon /></Avatar>} label={props.item.stargazers_count} />
-                            <Chip className="chip" avatar={<Avatar><FmdBadIcon /></Avatar>} label={props.item.open_issues} />
-                            <Chip className="chip" label={props.item.language ? props.item.language : `Not Specified`} />
+                            <Grid container justifyContent="space-between">
+                                <Grid item xs={12} md={6}>
+                                    <Chip className="chip" avatar={<Avatar><StarIcon /></Avatar>} label={props.item.stargazers_count} />
+                                    <Chip className="chip" avatar={<Avatar><FmdBadIcon /></Avatar>} label={props.item.open_issues} sx={{ ml: 2 }} />
+                                    <Chip className="chip" label={props.item.language ? props.item.language : `Not Specified`} sx={{ ml: 2 }} />
+                                </Grid>
+                                <Grid item xs={12} md={6} sx={{ mt: 2 }}>
+                                    <Chip variant="outlined" className="chip" label={`Submitted ${daysRemaining} days ago by ${props.item.owner.login}...`} />
+                                </Grid>
+                            </Grid>
                         </div>
                         <div className='footer'>
                             <Button variant='contained' className='button' onClick={handleButtonClick}>Read More</Button>
-                            <Chip variant="outlined" className="chip" label={`Submitted ${daysRemaining} days ago by ${props.item.owner.login}`} />
                         </div>
                     </Grid>
                 </Grid>
